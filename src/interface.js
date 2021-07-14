10226ccf77d7e4b8380a69c1acfadb0fb9a9ed41
Sean Phillips <seanphillips@Seans-Air.broadband>
2021-07-14 17:28:54 +0100

    trying to create html

--- src/interface.js
+++ src/interface.js
@@ -24,9 +24,39 @@ document.addEventListener("DOMContentLoaded", () => {
     note = new Note();
     note.createNote(noteText);
     list.insertNote(note);
-    document.querySelector(`#note-${noteNum.toString()}`).innerText = list.displayNotes();
-    document.querySelector(`#note-${noteNum.toString()}-full`).innerText = list.displayNote(0);
+    document.querySelector(`#note-1`).innerText = list.displayNotes();
+    document.querySelector(`#note-1-full`).innerText = list.displayNote(0);
     noteNum ++;
   })
 
+  const createElement = () => {
+    let newDiv = document.createElement('div');
+    let newButton = document.createElement('button');
+    newButton.id = ('note-1');
+    newButton.className = ('accordion');
+    newDiv.appendChild(newButton);
+    let newDivTwo = document.createElement('div');
+    newDivTwo.id = ('note-1-full');
+    newDivTwo.className = ('panel');
+    let emptyP = document.createElement('p');
+    newDivTwo.appendChild(emptyP);
+    newDiv.appendChild(newDivTwo);
+    let container = document.querySelector('body, .container');
+    container.append(newDiv);
+  }
+
+  createElement();
+  console.log(document.querySelector('#note-1'))
+
 })
+
+// document.querySelector('#note').addEventListener('submit', (event) => {
+//   event.preventDefault();
+//   const noteText = document.querySelector('#note-text').value;
+//   note = new Note();
+//   note.createNote(noteText);
+//   list.insertNote(note);
+//   document.querySelector(`#note-${noteNum.toString()}`).innerText = list.displayNotes();
+//   document.querySelector(`#note-${noteNum.toString()}-full`).innerText = list.displayNote(0);
+//   noteNum ++;
+// })
