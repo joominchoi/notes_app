b8d2d3a49ab0c29b576f9d8c42eb4cf9a81ebd0d
Sean Phillips <seanphillips@Seans-Air.broadband>
2021-07-14 19:32:02 +0100

    adding js for creating html elements

--- src/interface.js
+++ src/interface.js
@@ -1,21 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const list = new List();
 
-  var acc = document.getElementsByClassName("accordion");
-  var i;
-
-  for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var panel = this.nextElementSibling;
-      if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-      } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-      }
-    });
-  }
-
   let noteNum = 1;
 
   document.querySelector('#note').addEventListener('submit', (event) => {
@@ -24,30 +9,47 @@ document.addEventListener("DOMContentLoaded", () => {
     note = new Note();
     note.createNote(noteText);
     list.insertNote(note);
-    document.querySelector(`#note-1`).innerText = list.displayNotes();
-    document.querySelector(`#note-1-full`).innerText = list.displayNote(0);
+    createNoteElement();
+    classEventListener();
+    document.querySelector(`#note-${noteNum.toString()}`).innerText = list.displayNotes();
+    document.querySelector((`#note-${noteNum.toString()}-full`)).innerText = list.displayNote(0);
     noteNum ++;
   })
 
-  const createElement = () => {
+  const createNoteElement = () => {
     let newDiv = document.createElement('div');
     let newButton = document.createElement('button');
-    newButton.id = ('note-1');
+    newButton.id = (`note-${noteNum.toString()}`);
     newButton.className = ('accordion');
     newDiv.appendChild(newButton);
     let newDivTwo = document.createElement('div');
-    newDivTwo.id = ('note-1-full');
+    newDivTwo.id = (`note-${noteNum.toString()}-full`);
     newDivTwo.className = ('panel');
     let emptyP = document.createElement('p');
     newDivTwo.appendChild(emptyP);
     newDiv.appendChild(newDivTwo);
-    let container = document.querySelector('body, .container');
-    container.append(newDiv);
+    let container = document.querySelector('.grid-container');
+    container.insertAdjacentElement('beforeend', newDiv);
   }
 
-  createElement();
-  console.log(document.querySelector('#note-1'))
 
+  const classEventListener = () => {
+    let acc = document.getElementsByClassName("accordion");
+    let i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+      });
+    }
+  }
+  
 })
 
 // document.querySelector('#note').addEventListener('submit', (event) => {
